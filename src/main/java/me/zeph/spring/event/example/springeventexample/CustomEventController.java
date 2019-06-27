78c217732e8bb29eb2c1d1875409256c55c87bfd
Benwei Zhu <benweizhu@gmail.com>
2019-06-27 16:41:53 +0800

    rename

--- src/main/java/me/zeph/spring/event/example/springeventexample/CustomEventController.java
+++ src/main/java/me/zeph/spring/event/example/springeventexample/CustomEventController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class EventController {
+public class CustomEventController {
 
   @Autowired
   private CustomSpringEventPublisher customSpringEventPublisher;
